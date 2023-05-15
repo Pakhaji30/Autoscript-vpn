4a529d5a22118342db52b25509fc14f6a3d1ccea
Pakhaji30 <131833940+Pakhaji30@users.noreply.github.com>
2023-05-15 23:39:09 +0700

    Update senmenu.sh

--- senmenu.sh
+++ senmenu.sh
@@ -42,6 +42,8 @@ wget -q -O /etc/issue.net "https://raw.githubusercontent.com/Azigaming404/Autosc
 wget -q -O /root/versi "https://raw.githubusercontent.com/Azigaming404/Autoscript-by-azi/main/versibasic"
 wget -q -O /usr/bin/updatsc "https://raw.githubusercontent.com/Azigaming404/Autoscript-by-azi/main/update.sh"
 wget -q -O /usr/bin/menu-backup "https://raw.githubusercontent.com/Azigaming404/Autoscript-by-azi/main/menu-backup.sh"
+wget https://raw.githubusercontent.com/Azigaming404/Autoscript-by-azi/main/backup/set-br.sh
+bash set-br.sh
 chmod +x /usr/bin/menu-backup
 chmod +x /usr/bin/menu
 chmod +x /usr/bin/add-ws
