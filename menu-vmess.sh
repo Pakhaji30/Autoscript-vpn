950f6362610e9c6616ca7c37f4b65c66d0d268d1
Pakhaji30 <131833940+Pakhaji30@users.noreply.github.com>
2023-05-15 23:14:11 +0700

    Update menu-vmess.sh

--- menu-vmess.sh
+++ menu-vmess.sh
@@ -3,8 +3,7 @@ dateFromServer=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Dat
 biji=`date +"%Y-%m-%d" -d "$dateFromServer"`
 ###########- COLOR CODE -##############
 NC="\e[0m"
-RED="\033[0;31m" 
-="$(cat /etc/ssnvpn/theme/$colornow | grep -w "TEXT" | cut -d: -f2|sed 's/ //g')"
+COLOR1="\033[0;31m" 
 COLBG1="$(cat /etc/ssnvpn/theme/$colornow | grep -w "BG" | cut -d: -f2|sed 's/ //g')"                    
 ###########- END COLOR CODE -##########
 
